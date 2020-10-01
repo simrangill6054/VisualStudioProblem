0b9ea96f30d803daec97e5f0ff62418e5f6ef9e5
simrangill6054 <59968848+simrangill6054@users.noreply.github.com>
2020-10-01 17:30:44 -0400

    Add files via upload

---
+++ class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace thirdClass.Models
+{
+    public class class1
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public double grades { get; set; }
+        public string email { get; set; }
+    }
+}
